b4d1239c295aa7931353c7002c9f6726a41dd536
Liam Dyer <liamcdyer@gmail.com>
2026-04-24 11:13:17 -0400

    feat(native): use builtin `mkdirp`

--- lua/blink/lib/native/init.lua
+++ lua/blink/lib/native/init.lua
@@ -62,7 +62,7 @@ end
 --- @param path string Where to save the library
 --- @param callback fun(err: string)
 function native.download(url, path, callback)
-  vim.fn.mkdir(vim.fs.dirname(path), 'p')
+  native.mkdirp(vim.fs.dirname(path))
   vim.net.request(url, { outpath = path }, function(err) callback(err) end)
 end
 
