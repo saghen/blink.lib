da71eb98e6252a8206f62a86eb1d27a21186cdae
phanium <91544758+phanen@users.noreply.github.com>
2026-04-24 21:17:52 +0800

    fix: tags -> optional match

--- lua/blink/lib/native/init.lua
+++ lua/blink/lib/native/init.lua
@@ -189,11 +189,13 @@ function native.try_git_commit(path)
 end
 
 --- @param path string Path to the repository root or some path inside the repository
---- @param tag string Tag to check for existence (e.g. 'v1.*')
+--- @param match? string Tag to check for existence (e.g. 'v1.*')
 --- @return string? tag For example 'v0.0.1'
-function native.git_tag(path, tag)
+function native.git_tag(path, match)
   path = vim.fs.root(path, '.git') or path
-  local process = vim.system({ 'git', 'describe', '--tags', '--match', tag }, { cwd = path }):wait(1000)
+  local cmd = match and { 'git', 'describe', '--tags', '--match', match }
+    or { 'git', 'describe', '--tags', '--exact-match' }
+  local process = vim.system(cmd, { cwd = path }):wait(1000)
   if process.code == 0 and process.stdout then
     local version = vim.version.parse(process.stdout)
     if version then return ('v%d.%d.%d'):format(version.major, version.minor, version.patch) end
